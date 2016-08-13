ea9af16510e519609768f34d9fa5e7a585027f5c
crazy-Q <qmx754@163.com>
2016-08-13 09:02:26 +0800

    添加图片

--- app/src/main/java/com/jikexueyuan/notebook/AddContent.java
+++ app/src/main/java/com/jikexueyuan/notebook/AddContent.java
@@ -1,8 +1,14 @@
 package com.jikexueyuan.notebook;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,13 +16,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.VideoView;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * Created by QQQ on 2016/8/12.
  */
-public class AddContent extends AppCompatActivity implements View.OnClickListener{
+public class AddContent extends AppCompatActivity implements View.OnClickListener {
 
     private String val;
     private Button savebtn, deletebtn;
@@ -25,6 +32,7 @@ public class AddContent extends AppCompatActivity implements View.OnClickListene
     private VideoView v_video;
     private NotesDB notesDB;
     private SQLiteDatabase dbWriter;
+    private File photoFile,videoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +52,27 @@ public class AddContent extends AppCompatActivity implements View.OnClickListene
 
         savebtn.setOnClickListener(this);
         deletebtn.setOnClickListener(this);
+
+        initView();
+    }
+
+    public void initView() {
+        if (val.equals("1")) {//添加的是文字
+            c_img.setVisibility(View.GONE);
+            v_video.setVisibility(View.GONE);
+        }
+        if (val.equals("2")) {//图片
+            c_img.setVisibility(View.VISIBLE);
+            v_video.setVisibility(View.GONE);
+            Intent imgIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            photoFile = new File(Environment.getExternalStorageDirectory().getAbsoluteFile() + "/" + getTime() + ".jpg");
+            imgIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+            startActivityForResult(imgIntent, 1);
+        }
+        if (val.equals("3")) {
+            c_img.setVisibility(View.GONE);
+            v_video.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -59,16 +88,26 @@ public class AddContent extends AppCompatActivity implements View.OnClickListene
         }
     }
 
-    public void addDB(){
+    public void addDB() {
         ContentValues cv = new ContentValues();
         cv.put(NotesDB.CONTENT, etText.getText().toString());
-        cv.put(NotesDB.TIME,getTime());
+        cv.put(NotesDB.TIME, getTime());
+        cv.put(NotesDB.PATH, photoFile + "");
         dbWriter.insert(NotesDB.TABLE_NAME, null, cv);
     }
 
-    private String getTime(){
+    private String getTime() {
         SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
         Date date = new Date();
         return format.format(date);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == 1) {
+            Bitmap bitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            c_img.setImageBitmap(bitmap);
+        }
     }
 }
--- app/src/main/java/com/jikexueyuan/notebook/MyAdapter.java
+++ app/src/main/java/com/jikexueyuan/notebook/MyAdapter.java
@@ -2,6 +2,10 @@ package com.jikexueyuan.notebook;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +13,6 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 /**
  * Created by QQQ on 2016/8/12.
@@ -47,13 +50,50 @@ public class MyAdapter extends BaseAdapter {
         TextView contentTv = (TextView) layout.findViewById(R.id.id_list_content);
         TextView timeTv = (TextView) layout.findViewById(R.id.id_list_time);
         ImageView imgIv = (ImageView) layout.findViewById(R.id.id_list_img);
-        VideoView videoIv = (VideoView) layout.findViewById(R.id.id_list_video);
+        ImageView videoIv = (ImageView) layout.findViewById(R.id.id_list_video);
 
         cursor.moveToPosition(i);
         String content = cursor.getString(cursor.getColumnIndex("content"));
         String time = cursor.getString(cursor.getColumnIndex("time"));
+        String url = cursor.getString(cursor.getColumnIndex("path"));
         contentTv.setText(content);
         timeTv.setText(time);
+        imgIv.setImageBitmap(getImageThumbnail(url,200,200));
         return layout;
     }
+
+    /**
+     * 获取缩略图
+     * @param uri
+     * @param width
+     * @param height
+     * @return
+     */
+    public Bitmap getImageThumbnail(String uri, int width, int height) {
+        Bitmap bitmap = null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        //第一次获取的bitmap实际上为null，只是为了读取宽度和高度
+        bitmap = BitmapFactory.decodeFile(uri, options);
+        options.inJustDecodeBounds = false;
+        // 计算缩放比
+        int beWidth = options.outWidth / width;
+        int beHeight = options.outHeight / height;
+        int be = 1;
+        if (beWidth < beHeight) {
+            be = beWidth;
+        } else {
+            be = beHeight;
+        }
+        if (be <= 0) {
+            be = 1;
+        }
+        Log.d("TAG", be + "");
+        options.inSampleSize = be;
+        bitmap = BitmapFactory.decodeFile(uri, options);
+        bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height,
+                ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+        return bitmap;
+    }
+
 }
